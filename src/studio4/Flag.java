86ee40c76d252686c77d9b2dda7d9a934a2382e0
sophieyoshino <sophieyoshino@localhost>
2023-10-04 15:49:25 -0500

    flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,15 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+	StdDraw.setPenColor(StdDraw.BLUE);
+	StdDraw.filledRectangle(0.5, 0.5, 0.45, 0.25);
+	StdDraw.setPenColor(StdDraw.WHITE);
+	StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.025);
+	StdDraw.filledRectangle(0.35, 0.25, 0.025, 0.5);
+	StdDraw.setPenColor(StdDraw.YELLOW);
+	
+//	double[] x =
+//	double[] y = 
+	//StdDraw.filledPolygon()
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,45 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		while(in.hasNext())
+		{
+			//String file = in.next();
+			System.out.println(f);
+			String shapeType = in.next();
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.nextBoolean();
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			
+			
+			
+			if (shapeType.equals ("rectangle"))
+			{
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			
+			if (shapeType.equals("ellipse"))
+			{
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			if (shapeType.equals("triangle"))
+			{
+				double parameterFive = in.nextDouble();
+				double parameterSix = in.nextDouble();
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				double[] x = {parameterOne, parameterThree, parameterFive};
+				double[] y = {parameterTwo, parameterFour, parameterSix};
+				StdDraw.filledPolygon(x, y);
+			}
+			
+			
+		}
+		
 	}
 }
